6f6f0efdd4bca94cd2d9de6ad9de580ac46c772a
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2023-03-30 22:06:13 -0300

    update messages endpoints

--- src/mutations/messages.ts
+++ src/mutations/messages.ts
@@ -13,8 +13,7 @@ const getMessages = async (contactId: number): Promise<Message[]> => {
 export const useGetMessagesMutation = () => useMutation(getMessages);
 
 const sendMessage = async (message: Message): Promise<number> => {
-  const response = await api.post('/messages/send', {
-    fromId: message.from.id,
+  const response = await api.post('/conversations/send-message', {
     toId: message.to.id,
     text: message.text,
     dateTime: message.dateTime,
@@ -29,7 +28,7 @@ export const useSendMessageMutation = () =>
   useMutation('sendMessage', sendMessage);
 
 const updateRead = async (contactId: number): Promise<void> => {
-  const response = await api.get(`/messages/${contactId}/update-read`);
+  const response = await api.get(`/conversations/${contactId}/update-read`);
   if (response.status !== 200) {
     throw new Error('An error occurred while updating message');
   }
